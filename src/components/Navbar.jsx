7b30ba8bcae9a292315eac327b30bcdfa4aa6c5f
Alex-Alessi <alex.alessi246@gmail.com>
2024-12-10 16:46:19 +0100

    work complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,43 @@
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  <nav className="navbar navbar-expand-lg bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        React-router
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link " aria-current="page" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/chi siamo">
-              Chi siamo
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/postsList">
-              Posts List
-            </NavLink>
-          </li>
-        </ul>
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          React-router
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/chi siamo">
+                Chi siamo
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/postsList">
+                Posts List
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>;
+    </nav>
+  );
 }
--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,7 +1,63 @@
 export default function PostsList() {
+  const cakes = [
+    {
+      id: 1,
+      name: "Torta al cioccolato",
+      ingredients: ["Farina", "Zucchero", "Cacao", "Burro", "Uova"],
+      calories: 450 + "kcal",
+      description:
+        "Un classico irresistibile, perfetta per gli amanti del cioccolato!",
+    },
+    {
+      id: 2,
+      name: "Cheesecake ai frutti di bosco",
+      ingredients: [
+        "Formaggio spalmabile",
+        "Panna",
+        "Biscotti",
+        "Frutti di bosco",
+      ],
+      calories: 350 + "kcal",
+      description: "Dolce e fresca, ideale per l'estate.",
+    },
+    {
+      id: 3,
+      name: "Torta di mele",
+      ingredients: ["Farina", "Zucchero", "Mele", "Cannella", "Burro"],
+      calories: 320 + "kcal",
+      description:
+        "Un dolce tradizionale, perfetto per accompagnare una tazza di tè.",
+    },
+    {
+      id: 4,
+      name: "Tiramisù",
+      ingredients: ["Mascarpone", "Savoiardi", "Caffè", "Cacao", "Zucchero"],
+      calories: 400 + "kcal",
+      description: "Un dolce al cucchiaio italiano famoso in tutto il mondo.",
+    },
+  ];
   return (
-    <div className="container">
-      <h1>Posts list</h1>
+    <div className="container py-4">
+      <h1 className="py-3">Lista Post</h1>
+      <ul>
+        {cakes.map((cake) => (
+          <li key={cake.id} className="py-2">
+            <h2>{cake.name}</h2>
+            <p>
+              <strong>Ingredienti: </strong>
+              {cake.ingredients.join(", ")}
+            </p>
+            <p>
+              <strong>Calorie: </strong>
+              {cake.calories}
+            </p>
+
+            <strong>Descrizione:</strong>
+
+            <p>{cake.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
